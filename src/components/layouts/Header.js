26b9f7220b34e8f9ce12280844b4b01c01aed46f
mmoramar <mariangel.moramarquez@everis.nttdata.com>
2022-12-13 16:19:28 -0500

    insert styles header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { Layout, Button, Avatar, Badge } from "antd";
-import { UserOutlined, ExportOutlined } from "@ant-design/icons";
+import { Layout,  Avatar, Badge } from "antd";
+import { QqOutlined } from "@ant-design/icons";
 import "./header.css";
+import logo from "../image/logo.png";
 
 const { Header } = Layout;
 
 export default function MainHeader() {
   return (
     <Header style={{ padding: 0 }}>
-      <div></div>
       <div className="user_section">
-        <Badge dot={true}>
-          <Avatar icon={<UserOutlined />} className="avatar-item" />
-        </Badge>
-        <span> Mariangel</span>
+        <img src={logo} alt="logo" />
         <div>
-          <Button type="link" className="btn-close">
-            <ExportOutlined />
-            &nbsp;Cerrar Sesión
-          </Button>
+          <Badge dot={true}>
+            <Avatar icon={<QqOutlined />} className="avatar-item" />
+          </Badge>
+          <span> Usuario</span> 
+
+      
         </div>
       </div>
     </Header>
