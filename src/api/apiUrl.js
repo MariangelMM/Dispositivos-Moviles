10c80edf9334cd4e678fa92c7da8f0e19fb0ab05
mmoramar <mariangel.moramarquez@everis.nttdata.com>
2022-12-19 14:21:38 -0500

    update url

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -2,15 +2,15 @@
 import axios from "axios";
 
 //Lectura de la variable de entorno
-const uri = "http://localhost:3001/api";
+const uri = process.env.BASE_URL || 'http://localhost:3001'
 
 
 export const getListProduct = ( ) => {
-  return axios.get( `${uri}/product` ).then( response => response.data );
+  return axios.get( `${uri}/api/product` ).then( response => response.data );
 }
 
 export const getDetailProduct = (id) => {    
-  return axios.get( `${uri}/product/${id}` ).then( response => response.data );
+  return axios.get( `${uri}/api/product/${id}` ).then( response => response.data );
 }
 
 export const postAddProduct = (data) => {  
@@ -20,6 +20,6 @@ export const postAddProduct = (data) => {
       accept: 'application/json',
     }
 	};
-  return axios.post( `${uri}/cart/`, data, config ).then( response => response.data );
+  return axios.post( `${uri}/api/cart/`, data, config ).then( response => response.data );
 
 }
